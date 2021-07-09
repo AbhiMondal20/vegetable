0bc37f47cee572331a18381845af671ba2797f30
abhijitsrd22 <85216013+abhijitsrd22@users.noreply.github.com>
2021-07-09 14:55:32 +0530

    Add files via upload

---
+++ Suggestions.js
@@ -0,0 +1,13 @@
+let suggestion=[
+    "Apple",
+    "Potato",
+    "Tomato",
+    "Chili",
+    "broccoli",
+    "cucumber",
+    "chicken",
+    "meat",
+    "hawaii",
+    "okra",
+    "Vegetables"
+]
